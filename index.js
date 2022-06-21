9301d888fc58adc54dcf76e26e20da4d5bd55148
João  Víctor Antunes <67442552+DevTrzJbr@users.noreply.github.com>
2022-06-21 15:02:09 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Intents } = require('discord.js');
+const token = process.env['TOKEN']
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on("messageCreate", (message) => {
+	if (message.author.bot) return;
+	
+	if (message.content === `ping`) {
+		message.channel.send(`Pong ${client.ws.ping}ms`)
+	}
+});
+
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.login(token);
